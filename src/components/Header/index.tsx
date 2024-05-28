60bdd5aea530eb9a876f181ed455f9fcd3c7fd88
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-05-28 14:09:39 -0300

    Adiciona o sistema de rotas para os produtos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header = () => {
           <nav>
             <Links>
               <LinkItem>
-                <Link to={'/categorias'}>Categorias</Link>
+                <Link to={'/categories'}>Categorias</Link>
               </LinkItem>
               <LinkItem>
                 <Link to={'#'}>Novidades</Link>
